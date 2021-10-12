fcf2cb145d0889ec910b3775943c323e89cfb0eb
54665 <45261464+54665@users.noreply.github.com>
2021-10-12 10:24:14 +0800

    Update aws.sh

--- aws.sh
+++ aws.sh
@@ -32,17 +32,17 @@ beikong0_chushihua(){
 		chkconfig vnstat on
 		vnstat -u -i eth0
 		mkdir /root/.awsll/
-		wget https://raw.githubusercontent.com/54665/yppro/master/vnstat.sh -O /root/.awsll/vnstat.sh
+		wget https://raw.githubusercontent.com/54665/yppro/main/vnstat.sh -O /root/.awsll/vnstat.sh
 		chmod +x /root/.awsll/vnstat.sh
-		wget https://raw.githubusercontent.com/54665/yppro/master/serviceoff.sh -O /root/.awsll/serviceoff.sh
+		wget https://raw.githubusercontent.com/54665/yppro/main/serviceoff.sh -O /root/.awsll/serviceoff.sh
 		chmod +x /root/.awsll/serviceoff.sh
-		wget https://raw.githubusercontent.com/54665/yppro/master/serviceon.sh -O /root/.awsll/serviceon.sh
+		wget https://raw.githubusercontent.com/54665/yppro/main/serviceon.sh -O /root/.awsll/serviceon.sh
 		chmod +x /root/.awsll/serviceon.sh
 		
 	read -p "是否下载300G版本脚本[y/N]" down_files_1
 	if [[ "$down_files_1" =~ ^[yY]$ ]]; then
 		echo "正在下载300G版脚本"
-		wget https://raw.githubusercontent.com/54665/yppro/master/300.py -O /root/.awsll/300.py
+		wget https://raw.githubusercontent.com/54665/yppro/main/300.py -O /root/.awsll/300.py
 		chmod +x /root/.awsll/300.py
 		nohup python /root/.awsll/300.py >> /dev/null 2>&1 &
 		
@@ -57,15 +57,15 @@ beikong0_chushihua(){
     rm -r "$file_1/crontab.bak"
     cron_config=$(crontab -l | grep "vnstat -u -i eth0")
     cron_config=$(crontab -l | grep "vnstat.sh")
-	wget https://raw.githubusercontent.com/54665/yppro/master/ds.txt -O /root/.awsll/ds.txt
+	wget https://raw.githubusercontent.com/54665/yppro/main/ds.txt -O /root/.awsll/ds.txt
 	crontab /root/.awsll/ds.txt
     echo "正在添加自启任务"
-    wget https://raw.githubusercontent.com/54665/yppro/master/300-zq.sh -O /root/.awsll/300-zq.sh
+    wget https://raw.githubusercontent.com/54665/yppro/main/300-zq.sh -O /root/.awsll/300-zq.sh
     chmod +x /root/.awsll/300-zq.sh
 	chmod +x /etc/rc.d/rc.local
 	echo "/root/.awsll/300-zq.sh >/root/.awsll/300.py.log 2>&1" >> /etc/rc.d/rc.local
 	echo "正在下载管理脚本"
-    curl -o /usr/bin/yp -Ls https://raw.githubusercontent.com/54665/yppro/master/ll.sh
+    curl -o /usr/bin/yp -Ls https://raw.githubusercontent.com/54665/yppro/main/ll.sh
 	chmod +x /usr/bin/yp
     echo "安装完成，请输入 yp 管理流量统计"
 	
@@ -84,17 +84,17 @@ beikong1_chushihua(){
 		chkconfig vnstat on
 		vnstat -u -i eth0
 		mkdir /root/.awsll/
-		wget https://raw.githubusercontent.com/54665/yppro/master/vnstat.sh -O /root/.awsll/vnstat.sh
+		wget https://raw.githubusercontent.com/54665/yppro/main/vnstat.sh -O /root/.awsll/vnstat.sh
 		chmod +x /root/.awsll/vnstat.sh
-		wget https://raw.githubusercontent.com/54665/yppro/master/serviceoff.sh -O /root/.awsll/serviceoff.sh
+		wget https://raw.githubusercontent.com/54665/yppro/main/serviceoff.sh -O /root/.awsll/serviceoff.sh
 		chmod +x /root/.awsll/serviceoff.sh
-		wget https://raw.githubusercontent.com/54665/yppro/master/serviceon.sh -O /root/.awsll/serviceon.sh
+		wget https://raw.githubusercontent.com/54665/yppro/main/serviceon.sh -O /root/.awsll/serviceon.sh
 		chmod +x /root/.awsll/serviceon.sh
 
 	read -p "是否下载1T版本脚本[y/N]" down_files_1
 	if [[ "$down_files_1" =~ ^[yY]$ ]]; then
 		echo "正在下载1T版脚本"
-		wget https://raw.githubusercontent.com/54665/yppro/master/1024.py -O /root/.awsll/1024.py
+		wget https://raw.githubusercontent.com/54665/yppro/main/1024.py -O /root/.awsll/1024.py
 		chmod +x /root/.awsll/1024.py
 		nohup python /root/.awsll/1024.py >> /dev/null 2>&1 &
 		
@@ -109,15 +109,15 @@ beikong1_chushihua(){
     rm -r "$file_1/crontab.bak"
     cron_config=$(crontab -l | grep "vnstat -u -i eth0")
     cron_config=$(crontab -l | grep "vnstat.sh")
-	wget https://raw.githubusercontent.com/54665/yppro/master/ds.txt -O /root/.awsll/ds.txt
+	wget https://raw.githubusercontent.com/54665/yppro/main/ds.txt -O /root/.awsll/ds.txt
 	crontab /root/.awsll/ds.txt
     echo "正在添加自启任务"
-    wget https://raw.githubusercontent.com/54665/yppro/master/1024-zq.sh -O /root/.awsll/1024-zq.sh
+    wget https://raw.githubusercontent.com/54665/yppro/main/1024-zq.sh -O /root/.awsll/1024-zq.sh
     chmod +x /root/.awsll/1024-zq.sh
 	chmod +x /etc/rc.d/rc.local
 	echo "/root/.awsll/1024-zq.sh >/root/.awsll/1024.py.log 2>&1" >> /etc/rc.d/rc.local
 	echo "正在下载管理脚本"
-    curl -o /usr/bin/yp -Ls https://raw.githubusercontent.com/54665/yppro/master/ll.sh
+    curl -o /usr/bin/yp -Ls https://raw.githubusercontent.com/54665/yppro/main/ll.sh
 	chmod +x /usr/bin/yp
     echo "安装完成，请输入 yp 管理流量统计"
 }
@@ -135,17 +135,17 @@ beikong2_chushihua(){
 		chkconfig vnstat on
 		vnstat -u -i eth0
 		mkdir /root/.awsll/
-		wget https://raw.githubusercontent.com/54665/yppro/master/vnstat.sh -O /root/.awsll/vnstat.sh
+		wget https://raw.githubusercontent.com/54665/yppro/main/vnstat.sh -O /root/.awsll/vnstat.sh
 		chmod +x /root/.awsll/vnstat.sh
-		wget https://raw.githubusercontent.com/54665/yppro/master/serviceoff.sh -O /root/.awsll/serviceoff.sh
+		wget https://raw.githubusercontent.com/54665/yppro/main/serviceoff.sh -O /root/.awsll/serviceoff.sh
 		chmod +x /root/.awsll/serviceoff.sh
-		wget https://raw.githubusercontent.com/54665/yppro/master/serviceon.sh -O /root/.awsll/serviceon.sh
+		wget https://raw.githubusercontent.com/54665/yppro/main/serviceon.sh -O /root/.awsll/serviceon.sh
 		chmod +x /root/.awsll/serviceon.sh
 
 	read -p "是否下载5G版本脚本[y/N]" down_files_1
 	if [[ "$down_files_1" =~ ^[yY]$ ]]; then
 		echo "正在下载5G版脚本"
-		wget https://raw.githubusercontent.com/54665/yppro/master/aws5.py -O /root/.awsll/aws5.py
+		wget https://raw.githubusercontent.com/54665/yppro/main/aws5.py -O /root/.awsll/aws5.py
 		chmod +x /root/.awsll/aws5.py
 		nohup python /root/.awsll/aws5.py >> /dev/null 2>&1 &
 		
@@ -161,12 +161,12 @@ beikong2_chushihua(){
     cron_config=$(crontab -l | grep "vnstat -u -i eth0")
     cron_config=$(crontab -l | grep "vnstat.sh")
     echo "正在添加自启任务"
-    wget https://raw.githubusercontent.com/54665/yppro/master/aws5-zq.sh -O /root/.awsll/aws5-zq.sh
+    wget https://raw.githubusercontent.com/54665/yppro/main/aws5-zq.sh -O /root/.awsll/aws5-zq.sh
     chmod +x /root/.awsll/aws5-zq.sh
 	chmod +x /etc/rc.d/rc.local
 	echo "/root/.awsll/aws5-zq.sh >/root/.awsll/aws5.py.log 2>&1" >> /etc/rc.d/rc.local
 	echo "正在下载管理脚本"
-    curl -o /usr/bin/aws -Ls https://raw.githubusercontent.com/54665/yppro/master/ll.sh
+    curl -o /usr/bin/aws -Ls https://raw.githubusercontent.com/54665/yppro/main/ll.sh
 	chmod +x /user/bin/yp
     echo "安装完成，请输入 yp 管理流量统计"
 }
