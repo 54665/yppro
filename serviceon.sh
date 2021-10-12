bc84462a302b6293c89b40d7b26a64cd2fb4522e
54665 <45261464+54665@users.noreply.github.com>
2021-10-12 10:18:44 +0800

    Update serviceon.sh

--- serviceon.sh
+++ serviceon.sh
@@ -4,10 +4,12 @@
 #
 # Copyright (c) 2020.
 
+echo "开始清空流量统计"
+        rm -rf /var/lib/vnstat/eth0
 echo "开始创建流量统计"
         vnstat --create -i eth0
 		chkconfig vnstat on
         vnstat -u -i eth0
-echo "停止服务"
+echo "重启服务"
 service nginx restart
 service docker restart
